aa5cb2a0860784c14684f8a1cfa35fe1bc993fcf
Ali Khan <alikhan40310@gmail.com>
2022-04-14 13:42:59 +0500

    installing bootstrap

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Expense />
+  </div>
+</template>
+
+<script>
+// import Expense
+import Expense from './components/Expense.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Expense,
+  }
+}
+</script>
+
+// <style >
+/* // @import "~@/assets/scss/vendors/bootstrap-vue/index"; */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Expense.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="justify-content-center d-flex cold-12 align-center">
+        <b-container class="bv-example-row ">
+            <div class="d-flex gap-3 ">
+                <b-form-input class=" col-6 mr-2" v-model="expenseValue" placeholder="Enter your number" type="number">
+                </b-form-input>
+                <b-button @click="addExpense" variant="primary">Add Expense</b-button>
+            </div>
+                <p>{{ExpenseResult}}</p>
+
+        </b-container>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            expenseValue: '',
+            ExpenseResult: '',
+        }
+    },
+    methods: {
+       addExpense() {
+           this.ExpenseResult = this.expenseValue;
+       }
+    }
+
+}
+</script>
+
+<style>
+</style>
